2d770740884471a835a7ef32b2a4e0a55bc73a09
sirox548 <55093250+sirox548@users.noreply.github.com>
2020-11-05 08:25:20 -0500

    dy4

--- nandiagarden/pizza/models.py
+++ nandiagarden/pizza/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Size(models.Model):
+    title = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.title
+
+
+class Pizza(models.Model):
+    topping1 = models.CharField(max_length=60)
+    topping2 = models.CharField(max_length=60)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+
